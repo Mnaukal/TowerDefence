8086a804e09038441e639dc4faf9662b748cf88b
Mnaukal <michal.topfer@gmail.com>
2019-07-05 10:23:52 +0200

    Created path for enemies

---
+++ Assets/Scripts/GameControllerSingleton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// main GameController script, Singleton
+/// </summary>
+public class GameControllerS : MonoBehaviour
+{
+    // Singleton
+    private static GameControllerS _instance;
+
+    /// <summary>
+    /// Singleton Instance
+    /// </summary>
+    public static GameControllerS I { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    // Links to GameObjects
+    public GameObject Path;
+}
---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    public Transform[] Points;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        for (int i = 0; i < Points.Length - 1; i++)
+        {
+            if(Points[i] != null && Points[i+1] != null)
+                Gizmos.DrawLine(Points[i].position, Points[i + 1].position);
+        }
+    }
+
+}
