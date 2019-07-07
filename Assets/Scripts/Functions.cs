af176872967c147d89031296aaee2ad3d5ce5438
Mnaukal <michal.topfer@gmail.com>
2019-07-07 09:54:59 +0200

    Path graphics and collider

---
+++ Assets/Scripts/Functions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public static class Functions
+{
+    public static Vector2[] Vector3ToVector2(Vector3[] vectors)
+    {
+        return vectors.Select(v => new Vector2(v.x, v.y)).ToArray();
+    }
+}
--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -1,10 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Path : MonoBehaviour
 {
     public Transform[] Points;
+    private LineRenderer lineRenderer;
+    EdgeCollider2D edgeCollider;
+
+    void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        edgeCollider = GetComponent<EdgeCollider2D>();
+    }
+
+    void Start()
+    {
+        Vector3[] positions = Points.Select(p => p.position).ToArray();
+        lineRenderer.positionCount = positions.Length;
+        lineRenderer.SetPositions(positions);
+        edgeCollider.points = Functions.Vector3ToVector2(positions);
+    }
 
     void OnDrawGizmos()
     {
