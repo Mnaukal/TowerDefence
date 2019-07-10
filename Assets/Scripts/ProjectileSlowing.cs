12f7b2607155303273c2f056db934201c45eb1f2
Mnaukal <michal.topfer@gmail.com>
2019-07-10 21:42:41 +0200

    Towers with slowing projectiles

---
+++ Assets/Scripts/ProjectileSlowing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSlowing : Projectile
+{
+    [SerializeField]
+    private Slowdown Slowdown;
+    [SerializeField]
+    private float slowAmount = 0.5f;
+    [SerializeField]
+    private float slowTime = 3f;
+
+    public override void ProjectileHit(Enemy enemyHit)
+    {
+        Slowdown slowdown = Instantiate(Slowdown, enemyHit.transform);
+        slowdown.SlowDownEnemy(enemyHit, slowAmount, slowTime);
+
+        base.ProjectileHit(enemyHit);
+    }
+}
---
+++ Assets/Scripts/Slowdown.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Slows down a enemy
+/// </summary>
+public class Slowdown : MonoBehaviour
+{
+    private Enemy Enemy;
+    private float originalSpeed;
+    private float timer;
+
+    /// <summary>
+    /// Slows down an enemy
+    /// </summary>
+    /// <param name="enemy">Enemy to slow down</param>
+    /// <param name="slowAmount">Fraction between 0 and 1 which determines new speed of the enemy relative to its original speed</param>
+    public void SlowDownEnemy(Enemy enemy, float slowAmount, float slowTime)
+    {
+        if (slowAmount < 0 || slowAmount > 1)
+            throw new System.ArgumentException("slowAmount must be between 0 and 1", nameof(slowAmount));
+
+        foreach (Slowdown s in enemy.GetComponentsInChildren<Slowdown>())
+        {
+            if (s != this)
+                s.ResetSpeed();
+        }
+
+        Enemy = enemy;
+        originalSpeed = enemy.Speed;
+        enemy.Speed *= slowAmount;
+        timer = slowTime;
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+            ResetSpeed();
+    }
+
+    private void ResetSpeed()
+    {
+        Enemy.Speed = originalSpeed;
+        Destroy(this.gameObject);
+    }
+}
